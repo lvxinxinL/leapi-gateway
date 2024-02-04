8b647c632ff1607efbc825080c526f957d7550c1
Ghost <2089049079@qq.com>
2024-02-04 23:35:53 +0800

    Initial commit

---
+++ src/main/java/com/ghost/leapigateway/LeapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ghost.leapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeapiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghost/leapigateway/LeapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.leapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
