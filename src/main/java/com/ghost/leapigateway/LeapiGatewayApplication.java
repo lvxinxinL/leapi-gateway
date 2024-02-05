96b0323de8791db7e189e4841a0081f0bf0487c9
Ghost <2089049079@qq.com>
2024-02-05 09:34:17 +0800

    test

--- src/main/java/com/ghost/leapigateway/LeapiGatewayApplication.java
+++ src/main/java/com/ghost/leapigateway/LeapiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.ghost.leapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LeapiGatewayApplication {
@@ -10,4 +13,13 @@ public class LeapiGatewayApplication {
         SpringApplication.run(LeapiGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/baidu")
+                        .uri("https://baidu.com"))
+                .route("host_route", r -> r.path("/lexiaoxin")
+                        .uri("https://blog.csdn.net/m0_74059961?spm=1011.2266.3001.5343"))
+                .build();
+    }
 }
