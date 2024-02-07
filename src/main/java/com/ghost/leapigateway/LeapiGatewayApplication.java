8a3a9b7030c719e6ee669f1cb63aa529e433ee89
Ghost <2089049079@qq.com>
2024-02-07 20:59:18 +0800

    add 引入 Dubbo 整合 Nacos 搭建注册中心实现 RPC

--- src/main/java/com/ghost/leapigateway/LeapiGatewayApplication.java
+++ src/main/java/com/ghost/leapigateway/LeapiGatewayApplication.java
@@ -1,16 +1,37 @@
 package com.ghost.leapigateway;
 
+import com.ghost.leapi.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableDubbo
+@Service
 public class LeapiGatewayApplication {
 
+    @DubboReference
+    private DemoService demoService;
+
     public static void main(String[] args) {
-        SpringApplication.run(LeapiGatewayApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(LeapiGatewayApplication.class, args);
+        LeapiGatewayApplication application = context.getBean(LeapiGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
+    }
+
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
     }
 
 //    @Bean
